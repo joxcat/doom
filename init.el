ed56791dea8537890893f50e3fcd64310a59d0c1
Johan Planchon <johan.planchon@simplx.fr>
2021-02-09 17:16:01 +0100

    bump

--- init.el
+++ init.el
@@ -146,7 +146,7 @@
        ;; (nim +lsp)               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty +dragndrop +gnuplot +present +jupyter)                 ; organize your plain life in plain text
+       (org +pretty +dragndrop +gnuplot +present +jupyter +pandoc +brain)                 ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
