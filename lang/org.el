20ad14f01b9b970febdc713367e43857ae6292eb
Johan Planchon <johan.planchon@simplx.fr>
2022-07-01 16:32:22 +0200

    fix: fixed org config error

--- lang/org.el
+++ lang/org.el
@@ -14,7 +14,7 @@
     ("~" (:foreground "RoyalBlue1"))
     ("+" (:strike-through t :foreground "gray30"))))
 
-(setq org-hide-block-startup t)
+;; (setq org-hide-block-startup t)
 ;; (setq org-hide-emphasis-markers t)
 
 (use-package! org-roam
